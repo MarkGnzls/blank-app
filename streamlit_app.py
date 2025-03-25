952d0b815af35e6a7812c6bc838284f1d07f55a7
MarkGnzls <angelogonzales257@gmail.com>
2025-03-25 15:40:36 +0800

    changes commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new passsdsdsdrgerg")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Title of the app
+st.title("🎈 My Interactive Web App")
+
+# Text input for user name
+user_name = st.text_input("Enter your name:")
+
+# Slider for selecting a number
+number = st.slider("Select a number", 0, 100, 50)
+
+# Button to submit the input
+if st.button("Submit"):
+    st.write(f"Hello, {user_name}! You selected the number {number}.")
+else:
+    st.write("Please enter your name and select a number.")
+
+# Additional interactive element: Checkbox
+if st.checkbox("Show more options"):
+    st.write("You can add more features here!")
